80174e2410b9e79167f1a2bf71b73f25416e4241
megha-1299 <116376023+megha-1299@users.noreply.github.com>
2025-07-10 14:30:54 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,7 @@
+variable "aws_region"     { default = "us-east-1" }
+variable "cluster_name"   { default = "eks-demo"  }
+variable "desired_nodes"  { default = 2 }
+variable "max_nodes"      { default = 3 }
+variable "min_nodes"      { default = 1 }
+variable "node_instance"  { default = "t3.medium" }
 
