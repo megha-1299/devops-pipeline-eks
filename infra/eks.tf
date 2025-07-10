ed13b94296a2b4acf792728d90c34aa3ba8fdf9e
megha-1299 <116376023+megha-1299@users.noreply.github.com>
2025-07-10 14:31:44 +0530

    Update eks.tf

--- infra/eks.tf
+++ infra/eks.tf
@@ -1,1 +1,19 @@
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.21.0"
+
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnet_ids      = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+
+  eks_managed_node_groups = {
+    default = {
+      desired_size = var.desired_nodes
+      max_size     = var.max_nodes
+      min_size     = var.min_nodes
+      instance_types = [var.node_instance]
+    }
+  }
+}
 
