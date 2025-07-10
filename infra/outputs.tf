e66ba9753502b8301ecd47b643d77d6f6518ea82
megha-1299 <116376023+megha-1299@users.noreply.github.com>
2025-07-10 14:32:09 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,5 @@
+output "cluster_name"       { value = module.eks.cluster_name }
+output "cluster_endpoint"   { value = module.eks.cluster_endpoint }
+output "cluster_ca"         { value = module.eks.cluster_certificate_authority_data }
+output "kubeconfig_file"    { value = module.eks.kubeconfig_filename }
 
