d55843df1e4f7231d488b57c9430ff2142fb5c24
megha-1299 <116376023+megha-1299@users.noreply.github.com>
2025-07-10 14:29:10 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (_, res) => res.send('👋 Hello from EKS + Node.js!'));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`App listening on ${PORT}`));
 
